90333840f31d4539594bf7e229d6da7570b988f4
Steve Jihyeok <saurus@saurusui-MacBook-Pro.local>
2016-08-31 20:05:50 +0900

    Initial commit

---
+++ app/src/main/java/inha/csi/MainActivity.java
@@ -0,0 +1,13 @@
+package inha.csi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
