9b997eea75d7b780fdc5486573f1e2aaff79a75a
saurus2 <saurus2@naver.com>
2016-10-02 13:58:22 +0900

    Adding Overlay about fifth building //adding explain text

--- app/src/main/java/csi/testapp/MainActivity.java
+++ app/src/main/java/csi/testapp/MainActivity.java
@@ -10,6 +10,7 @@ import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -25,37 +26,46 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.skp.Tmap.TMapGpsManager;
 import com.skp.Tmap.TMapPoint;
 import com.skp.Tmap.TMapView;
 import com.skp.Tmap.TMapGpsManager.onLocationChangedCallback;
 
-public class MainActivity extends AppCompatActivity implements onLocationChangedCallback,LocationListener{
+public class MainActivity extends AppCompatActivity implements onLocationChangedCallback, LocationListener {
 
     public static String mApiKey = "8bdbb125-7d59-3684-84ff-ad4b5bb59e74";
     private TMapView mMapView = null;
     private TMapPoint n_Locate = null;
-    private RelativeLayout mMainRelativeLayout=null;
+    private RelativeLayout mMainRelativeLayout = null;
     TMapGpsManager gps;
     final int READ_ROCATE_CODE = 0;
 
     private LocationManager locationManager;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
-    private void configureMapView(){
+    private void configureMapView() {
         mMapView.setSKPMapApiKey(mApiKey);
     }
 
-    void initView(){
+    void initView() {
         mMapView = new TMapView(this);
         mMapView.setCenterPoint(126.65318, 37.449666);
         mMapView.setLocationPoint(126.65318, 37.449666);
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
         mMapView.setLanguage(TMapView.LANGUAGE_KOREAN);
         mMapView.setIcon(bitmap);
         mMapView.setIconVisibility(true);
         mMapView.setSightVisible(true);
         mMapView.setZoomLevel(17);
-        gps=new TMapGpsManager(this);
+        gps = new TMapGpsManager(this);
     }
 
 
@@ -63,7 +73,7 @@ public class MainActivity extends AppCompatActivity implements onLocationChanged
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mMainRelativeLayout = (RelativeLayout)findViewById(R.id.mainRelativeLayout);
+        mMainRelativeLayout = (RelativeLayout) findViewById(R.id.mainRelativeLayout);
         initView();
         mMainRelativeLayout.addView(mMapView);
         configureMapView();
@@ -71,7 +81,9 @@ public class MainActivity extends AppCompatActivity implements onLocationChanged
         findViewById(R.id.button).setOnClickListener(handler);
 
 
-
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     View.OnClickListener handler = new View.OnClickListener() {
@@ -82,43 +94,54 @@ public class MainActivity extends AppCompatActivity implements onLocationChanged
 
     };
 
-    void settingGPS(){
+//    화면변환하는 버튼 onclick 활성화 xml 의 디자인에서 수정함
+//    버튼 이벤트 자바에 미리 만들어 놓기 -> 엑티비티 연결된 xml에서 버튼 생성 -> 버튼 프로퍼티에서 Onclick 옵션에 추가
+    public void clicked2(View v) {
+        Intent intent = new Intent(getApplicationContext(), NextActivity.class);
+        startActivity(intent);
+    }
+
+    void settingGPS() {
         gps.setMinTime(1000);
         gps.setMinDistance(5);
         gps.setProvider(gps.GPS_PROVIDER);
         gps.OpenGps();
-    };
+    }
 
-    void doAction(){
-        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,2000,10,this);
+    ;
 
-    };
-
-    @Override
-    public void onLocationChange(Location location){
+    void doAction() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, this);
 
     }
 
-    void tryCheckPermission(){
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission_group.LOCATION) == PackageManager.PERMISSION_GRANTED){
+    ;
+
+    @Override
+    public void onLocationChange(Location location) {
+
+    }
+
+    void tryCheckPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission_group.LOCATION) == PackageManager.PERMISSION_GRANTED) {
             settingGPS();
-        }else{
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission_group.LOCATION},READ_ROCATE_CODE);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission_group.LOCATION}, READ_ROCATE_CODE);
         }
     }
 
-   @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
-       super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-       switch(requestCode){
-           case READ_ROCATE_CODE:
-               if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
-                   settingGPS();
-               }
-       }
-   }
+        switch (requestCode) {
+            case READ_ROCATE_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    settingGPS();
+                }
+        }
+    }
 
 
     @Override
@@ -150,5 +173,41 @@ public class MainActivity extends AppCompatActivity implements onLocationChanged
         startActivity(intent);
         Toast.makeText(getBaseContext(), "Gps is turned off!! ",
                 Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
