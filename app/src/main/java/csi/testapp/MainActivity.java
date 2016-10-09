4eda0bd05fbd942dd78dd652c99ca421b592ea91
saurus2 <saurus2@naver.com>
2016-10-10 00:03:17 +0900

    test

--- app/src/main/java/csi/testapp/MainActivity.java
+++ app/src/main/java/csi/testapp/MainActivity.java
@@ -10,6 +10,7 @@ import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -19,57 +20,52 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.skp.Tmap.TMapGpsManager;
 import com.skp.Tmap.TMapPoint;
-import com.skp.Tmap.TMapPolyLine;
 import com.skp.Tmap.TMapView;
 import com.skp.Tmap.TMapGpsManager.onLocationChangedCallback;
-import com.skp.Tmap.TMapData;
-import com.skp.Tmap.TMapData.BizCategoryListenerCallback;
-import com.skp.Tmap.TMapData.ConvertGPSToAddressListenerCallback;
-import com.skp.Tmap.TMapData.FindAllPOIListenerCallback;
-import com.skp.Tmap.TMapData.FindAroundNamePOIListenerCallback;
-import com.skp.Tmap.TMapData.FindPathDataAllListenerCallback;
-import com.skp.Tmap.TMapData.FindPathDataListenerCallback;
-import com.skp.Tmap.TMapData.TMapPathType;
 
-public class MainActivity extends AppCompatActivity implements onLocationChangedCallback,LocationListener{
+public class MainActivity extends AppCompatActivity implements onLocationChangedCallback, LocationListener {
 
     public static String mApiKey = "8bdbb125-7d59-3684-84ff-ad4b5bb59e74";
     private TMapView mMapView = null;
     private TMapPoint n_Locate = null;
-    private RelativeLayout mMainRelativeLayout=null;
+    private RelativeLayout mMainRelativeLayout = null;
     TMapGpsManager gps;
     final int READ_ROCATE_CODE = 0;
-    Bitmap bitmap;
-    Bitmap end;
-    private LocationManager locationManager;
 
-    private void configureMapView(){
+    private LocationManager locationManager;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    private void configureMapView() {
         mMapView.setSKPMapApiKey(mApiKey);
     }
 
-    void initView(){
+    void initView() {
         mMapView = new TMapView(this);
         mMapView.setCenterPoint(126.65318, 37.449666);
         mMapView.setLocationPoint(126.65318, 37.449666);
-        bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
-        end = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
         mMapView.setLanguage(TMapView.LANGUAGE_KOREAN);
         mMapView.setIcon(bitmap);
         mMapView.setIconVisibility(true);
         mMapView.setSightVisible(true);
         mMapView.setZoomLevel(17);
-        gps=new TMapGpsManager(this);
-        mMapView.setTMapPathIcon(bitmap,end);
-        mMapView.setCompassMode(true);
+        gps = new TMapGpsManager(this);
     }
 
 
@@ -77,23 +73,17 @@ public class MainActivity extends AppCompatActivity implements onLocationChanged
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mMainRelativeLayout = (RelativeLayout)findViewById(R.id.mainRelativeLayout);
+        mMainRelativeLayout = (RelativeLayout) findViewById(R.id.mainRelativeLayout);
         initView();
         mMainRelativeLayout.addView(mMapView);
         configureMapView();
         tryCheckPermission();
         findViewById(R.id.button).setOnClickListener(handler);
 
-        //컴파스 버튼 테스트 부분
-        Button btn_compas = (Button)findViewById(R.id.compas);
-        btn_compas.setOnClickListener(new Button.OnClickListener(){
-            public void onClick(View v){
-                mMapView.setCompassMode(true);
-            }
-        });
 
-
-
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     View.OnClickListener handler = new View.OnClickListener() {
@@ -104,67 +94,58 @@ public class MainActivity extends AppCompatActivity implements onLocationChanged
 
     };
 
-    void settingGPS(){
+//    화면변환하는 버튼 onclick 활성화 xml 의 디자인에서 수정함
+//    버튼 이벤트 자바에 미리 만들어 놓기 -> 엑티비티 연결된 xml에서 버튼 생성 -> 버튼 프로퍼티에서 Onclick 옵션에 추가
+    public void clicked2(View v) {
+        Intent intent = new Intent(getApplicationContext(), NextActivity.class);
+        startActivity(intent);
+    }
+
+    void settingGPS() {
         gps.setMinTime(1000);
         gps.setMinDistance(5);
         gps.setProvider(gps.GPS_PROVIDER);
         gps.OpenGps();
-    };
-
-
-
-    void doAction(){
-
-        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,2000,10,this);
-
-    };
-
-    public void drawPedestrianPath(double s_lat, double s_long) {
-        TMapPoint point1 = new TMapPoint(s_lat,s_long);
-        TMapPoint point2 = new TMapPoint(37.442584,126.662710);
-
-        TMapData tmapdata = new TMapData();
-
-        tmapdata.findPathDataWithType(TMapPathType.PEDESTRIAN_PATH, point1, point2, new FindPathDataListenerCallback() {
-            @Override
-            public void onFindPathData(TMapPolyLine polyLine) {
-                polyLine.setLineColor(Color.BLUE);
-                mMapView.addTMapPath(polyLine);
-            }
-        });
     }
 
+    ;
+
+    void doAction() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, this);
+
+    }
+
+    ;
 
     @Override
-    public void onLocationChange(Location location){
+    public void onLocationChange(Location location) {
 
     }
 
-    void tryCheckPermission(){
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission_group.LOCATION) == PackageManager.PERMISSION_GRANTED){
+    void tryCheckPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission_group.LOCATION) == PackageManager.PERMISSION_GRANTED) {
             settingGPS();
-        }else{
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission_group.LOCATION},READ_ROCATE_CODE);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission_group.LOCATION}, READ_ROCATE_CODE);
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
-        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        switch(requestCode){
+        switch (requestCode) {
             case READ_ROCATE_CODE:
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
                     settingGPS();
                 }
         }
     }
 
-    int check = 0;
+
     @Override
     public void onLocationChanged(Location location) {
-
         double Latitude = location.getLatitude();
         double Longitude = location.getLongitude();
         String msg = "New Latitude: " + Latitude
@@ -173,10 +154,6 @@ public class MainActivity extends AppCompatActivity implements onLocationChanged
         Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
         mMapView.setCenterPoint(Longitude, Latitude);
         mMapView.setLocationPoint(Longitude, Latitude);
-        if(check == 0) {
-            drawPedestrianPath(Latitude, Longitude);
-            check++;
-        }
     }
 
     @Override
@@ -196,5 +173,41 @@ public class MainActivity extends AppCompatActivity implements onLocationChanged
         startActivity(intent);
         Toast.makeText(getBaseContext(), "Gps is turned off!! ",
                 Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
