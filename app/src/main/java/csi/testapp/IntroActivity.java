cc3e863cfb9a217d6897f498697d99976c5c745a
YoungDonJUNG <yellowjung@naver.com>
2016-11-03 22:47:46 +0900

    finish Intro

--- app/src/main/java/csi/testapp/IntroActivity.java
+++ app/src/main/java/csi/testapp/IntroActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 
@@ -12,32 +13,24 @@ import android.view.Window;
  * Created by saurus on 2016. 11. 2..
  */
 
-public class IntroActivity extends Activity {
-    Handler h;//핸들러 선언
+public class  IntroActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE); //인트로화면이므로 타이틀바를 없앤다
         setContentView(R.layout.main_logo);
-        h= new Handler(); //딜래이를 주기 위해 핸들러 생성
-        h.postDelayed(mrun, 2000); // 딜레이 ( 런어블 객체는 mrun, 시간 2초)
-    }
 
-    Runnable mrun = new Runnable(){
-        @Override
-        public void run(){
-            Intent i = new Intent(getApplicationContext(), MainActivity.class); //인텐트 생성(현 액티비티, 새로 실행할 액티비티)
-            startActivity(i);
-            finish();
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-            //overridePendingTransition 이란 함수를 이용하여 fade in,out 효과를줌. 순서가 중요
-        }
-    };
-    //인트로 중에 뒤로가기를 누를 경우 핸들러를 끊어버려 아무일 없게 만드는 부분
-    //미 설정시 인트로 중 뒤로가기를 누르면 인트로 후에 홈화면이 나옴.
-    @Override
-    public void onBackPressed(){
-        super.onBackPressed();
-        h.removeCallbacks(mrun);
+        //화면 전환 부분
+        Handler handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg){
+                startActivity(new Intent(IntroActivity.this,MainActivity.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        };
+        //3초 딜레이
+        handler.sendEmptyMessageDelayed(0, 1500);
     }
 }
